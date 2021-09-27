eddd96e46d709abb9a2b8d5437773d9c80fc1e63
TranNgocAnhThai2004110031 <anhthaii1810@gmail.com>
2021-09-27 08:18:40 +0700

    Nộp Bài Tập

---
+++ src/com/thaitna2004110031/Tuan03/Bai3Lab3.java
@@ -0,0 +1,18 @@
+package com.thaitna2004110031.tuan03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bai3Lab3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào số lượng phần tử của mảng: ");
+        int n = sc.nextInt();
+        double [] a = new double [n];
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Nhập vào phần tử thứ %d: ", + (i+1));
+            a[i] = sc.nextDouble();
+        }
+        System.out.println("Mảng vừa nhập là: " + Arrays.toString(a));
+    }
+}
