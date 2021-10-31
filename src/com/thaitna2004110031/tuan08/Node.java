32bd6bec98fc732f559806557c131ed393e43fa0
TranNgocAnhThai2004110031 <anhthaii1810@gmail.com>
2021-10-31 15:17:55 +0700

    Nộp Bài

---
+++ src/com/thaitna2004110031/tuan08/Node.java
@@ -0,0 +1,14 @@
+package com.thaitna2004110031.tuan08;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+    
+}
---
+++ src/com/thaitna2004110031/tuan08/Student.java
@@ -0,0 +1,9 @@
+package com.thaitna2004110031.tuan08;
+
+public class Student {
+    String tenSV;
+    Student(){}
+    Student(String tsv){
+        tenSV = tsv;
+    }
+}
